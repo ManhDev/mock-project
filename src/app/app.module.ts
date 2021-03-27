3673ce40b981e7126e769c3c52a03edd313d8871
Doan Manh <dvmanh96vp@gmail.com>
2021-03-27 09:27:01 +0700

    Home and Article-1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/screens/navbar/navbar.component';
+import { HomeComponent } from './components/screens/home/home.component';
+import { AddArticlesComponent } from './components/common/add-articles/add-articles.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AddArticlesComponent,
   ],
   imports: [
     BrowserModule,
