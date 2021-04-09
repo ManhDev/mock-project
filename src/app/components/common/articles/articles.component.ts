cfe48b79f3eae15b28deeebfcfd40f8b2e9960bb
Duc hihi <duc16022k@gmail.com>
2021-04-09 09:40:30 +0700

    complete_like

--- src/app/components/common/articles/articles.component.ts
+++ src/app/components/common/articles/articles.component.ts
@@ -2,40 +2,55 @@ import { Comment } from './../../../models/comment';
 import { CommentsService } from './../../../services/comments.service';
 import { Article } from '../../../models/article';
 import { Component, Input, OnInit } from '@angular/core';
-
-
+import { ArticlesService } from 'src/app/services/articles.service';
 
 @Component({
   selector: 'app-articles',
   templateUrl: './articles.component.html',
-  styleUrls: ['./articles.component.scss']
+  styleUrls: ['./articles.component.scss'],
 })
 export class ArticlesComponent implements OnInit {
-  @Input('article') article: Article
+  @Input('article') article: Article;
   comments: Comment[] = [];
   focusComment: boolean;
 
-  constructor(private commentsService: CommentsService) { }
+  constructor(
+    private commentsService: CommentsService,
+    private articleService: ArticlesService
+  ) {}
 
   ngOnInit(): void {
-
-
-    this.commentsService.getCommentBySlug(this.article.slug).subscribe(res => {
-      this.comments = res['comments']
-      console.log(this.comments);
-
-    })
+    this.commentsService
+      .getCommentBySlug(this.article.slug)
+      .subscribe((res) => {
+        this.comments = res['comments'];
+        console.log(this.comments);
+      });
   }
 
   addNewComment($event) {
-    this.commentsService.getCommentBySlug(this.article.slug).subscribe(res => {
-      this.comments = res['comments']
-    })
+    this.commentsService
+      .getCommentBySlug(this.article.slug)
+      .subscribe((res) => {
+        this.comments = res['comments'];
+      });
   }
-
 
   onComment() {
     this.focusComment = true;
   }
 
+  likeHandler(article) {
+    if (!article.favorited) {
+      this.articleService.like(article.slug).subscribe((res: any) => {
+        article.favorited = true;
+        article.favoritesCount++;
+      });
+    } else {
+      this.articleService.unLike(article.slug).subscribe((res: any) => {
+        article.favorited = false;
+        article.favoritesCount--;
+      });
+    }
+  }
 }
